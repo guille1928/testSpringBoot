325bbb283b2c8eab241fb7421edd89b5872041b7
Guille <150677095+guille1928@users.noreply.github.com>
2025-10-01 16:38:32 +0200

    array

---
+++ src/main/java/com/example/demo/CalculadoraService.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculadoraService {
+
+		public void setNumbers (ArrayList<Integer> numeros){
+			
+			
+			
+		
+		
+		}
+	
+	
+}
---
+++ src/main/java/com/example/demo/Numeros.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+
+public class Numeros {
+
+	private ArrayList<Integer> numeros = new ArrayList<>();
+
+	public Numeros (ArrayList<Integer> numeros) {
+		this.numeros= numeros;
+		
+	}
+
+	public ArrayList<Integer> getNumeros() {
+		return numeros;
+	}
+
+	public void setNumeros(ArrayList<Integer> numeros) {
+		this.numeros = numeros;
+	}
+	
+	
+	
+}
